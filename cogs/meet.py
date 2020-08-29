97f06afafbe4263850ce1dfcc6b285ff09915d1d
Samy Benmahrez <samyben152@gmail.com>
2020-08-29 12:27:36 +0100

    meet 5 update

--- cogs/meet.py
+++ cogs/meet.py
@@ -16,10 +16,9 @@ class Meet(commands.Cog):
         embed.set_footer(text='By @Co-Founders')
         embed.set_author(
             name='RPS Bot', icon_url='https://cdn.discordapp.com/attachments/734563166775935016/737464407629627492/Artboard_18.png')
-        embed.add_field(name='1- Defining our pillars:', value='Must have elements, More than a feature set\n You have to define what makes this pillars important\n Define in one sentence\n 3 to 5 design pillars\n They are no features\n What the consumer thinks of first on your title', inline=False)
-        embed.add_field(name='2- Talking about the Game loop:',
-                        value='Should be funny and rewarding\n Called addiction loop\n The crux of the experience\n Ex:  Clash of Clans -> Collecting Resources, Building & Training, Batting', inline=True)
-        embed.add_field(name='3- Magic Moments:', value='Shareable with friends\n Causes emotion\n Needs to be Specific\n On sentence definition\n No more than 3 - 5\n Ex: Mario Kart magic moments -> Cross a finish line (zoom of on the distance), Red shell Hit, Drift perfect', inline=False)
+        embed.add_field(name='4- Feature Set:', value='A List of all major Elements\n Can be non-functional things (build, tools and dev related stuff)\n Everything necessary to ship\n What matters most for every feature -> the main goal of the feature\n What Cannot be compromised\n Guide your decision making', inline=False)
+        embed.add_field(name='5- Elevator Pitch', value='', inline=True)
+        embed.add_field(name='6- Development:', value='https://www.youtube.com/watch?v=a7vZFiFLw-w', inline=False)
         await ctx.send(embed=embed)
 
 
