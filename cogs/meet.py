90b81fabfcd973585183fd84a346a5fc784114bc
Samy <46052152+samcodesign@users.noreply.github.com>
2020-11-24 14:13:40 +0100

    Update meet.py

--- cogs/meet.py
+++ cogs/meet.py
@@ -14,10 +14,10 @@ class Meet(commands.Cog):
             colour=discord.Colour.blurple()
         )
         embed.set_footer(text='By @Co-Founders')
-        embed.set_author(name='RPS Bot', icon_url='https://cdn.discordapp.com/attachments/734563166775935016/737464407629627492/Artboard_18.png')
-        embed.add_field(name='4- Feature Set:', value='A List of all major Elements\n Can be non-functional things (build, tools and dev related stuff)\n Everything necessary to ship\n What matters most for every feature -> the main goal of the feature\n What Cannot be compromised\n Guide your decision making', inline=False)
+        embed.set_author(name='RPS Bot', icon_url='')
+        embed.add_field(name='4- Feature Set:', value='', inline=False)
         embed.add_field(name='5- Elevator Pitch', value='nothing to add', inline=True)
-        embed.add_field(name='6- Development:', value='https://www.youtube.com/watch?v=a7vZFiFLw-w', inline=False)
+        embed.add_field(name='6- Development:', value='', inline=False)
         await ctx.send(embed=embed)
 
 
